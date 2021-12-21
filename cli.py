508846970cde52b70c212b6021694e59fc421466
Jacob Lewallen <jlewallen@gmail.com>
2021-12-21 06:33:39 -0800

    misc: QuantLib options stuff, just experimentation.

--- cli.py
+++ cli.py
@@ -1,6 +1,8 @@
 #!env/bin/python3
 
 import logging, asyncio, os, aiofiles.os, json
+import QuantLib as ql
+import matplotlib.pyplot as plt
 import finnhub
 from pytz import timezone
 from time import mktime
@@ -36,6 +38,76 @@ async def get_earnings_calendar():
                 await write_json_file(res, calendar_path)
 
         start += relativedelta(months=1)
+
+
+async def test():
+    maturity_date = ql.Date(15, 1, 2016)
+    spot_price = 170
+    strike_price = 165
+    volatility = 0.20  # the historical vols or implied vols
+    dividend_rate = None  # 0.0063
+    option_type = ql.Option.Call
+
+    risk_free_rate = 0.001
+    day_count = ql.Actual365Fixed()
+    calendar = ql.UnitedStates()
+
+    calculation_date = ql.Date(8, 5, 2015)
+    ql.Settings.instance().evaluationDate = calculation_date
+
+    payoff = ql.PlainVanillaPayoff(option_type, strike_price)
+    settlement = calculation_date
+
+    am_exercise = ql.AmericanExercise(settlement, maturity_date)
+    american_option = ql.VanillaOption(payoff, am_exercise)
+
+    eu_exercise = ql.EuropeanExercise(maturity_date)
+    european_option = ql.VanillaOption(payoff, eu_exercise)
+
+    spot_handle = ql.QuoteHandle(ql.SimpleQuote(spot_price))
+    flat_ts = ql.YieldTermStructureHandle(
+        ql.FlatForward(calculation_date, risk_free_rate, day_count)
+    )
+    flat_vol_ts = ql.BlackVolTermStructureHandle(
+        ql.BlackConstantVol(calculation_date, calendar, volatility, day_count)
+    )
+
+    if dividend_rate:
+        dividend_yield = ql.YieldTermStructureHandle(  # type: ignore
+            ql.FlatForward(calculation_date, dividend_rate, day_count)
+        )
+        bsm_process = ql.BlackScholesMertonProcess(
+            spot_handle, dividend_yield, flat_ts, flat_vol_ts
+        )
+    else:
+        bsm_process = ql.BlackScholesProcess(spot_handle, flat_ts, flat_vol_ts)
+
+    nsteps = 200
+    american_option.setPricingEngine(
+        ql.BinomialVanillaEngine(bsm_process, "crr", nsteps)
+    )
+    european_option.setPricingEngine(ql.AnalyticEuropeanEngine(bsm_process))
+
+    def binomial_price(option, bsm_process, steps):
+        option.setPricingEngine(ql.BinomialVanillaEngine(bsm_process, "crr", steps))
+        return option.NPV()
+
+    steps = range(5, nsteps, 1)
+    eu_prices = [binomial_price(european_option, bsm_process, step) for step in steps]
+    am_prices = [binomial_price(american_option, bsm_process, step) for step in steps]
+    bs_price = american_option.NPV()
+
+    log.info(f"{bs_price}")
+
+    # plt.plot(steps, eu_prices, label="European Option", lw=2, alpha=0.6)
+    plt.plot(steps, am_prices, label="American Option", lw=2, alpha=0.6)
+    plt.plot([5, 200], [bs_price, bs_price], "r--", label="BSM Price", lw=2, alpha=0.6)
+    plt.xlabel("Steps")
+    plt.ylabel("Price")
+    plt.ylim(6, 20)
+    plt.title("Binomial Tree Price For Varying Steps")
+    plt.legend()
+    plt.show()
 
 
 async def main():
@@ -76,12 +148,17 @@ async def main():
         log.info(f"closing")
         await db.close()
 
+    await test()
+
 
 if __name__ == "__main__":
     logging.basicConfig(
         level=logging.DEBUG,
         format="%(asctime)s %(levelname)7s %(name)s %(message)s",
     )
+
+    logging.getLogger("matplotlib.font_manager").setLevel(logging.INFO)
+    logging.getLogger("PIL.PngImagePlugin").setLevel(logging.INFO)
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
